7b5b8d14465c6e7fc738bced54105881ac3bd2bf
VenezzaX <piozada243@gmail.com>
2025-03-27 13:07:45 -0300

    Update and rename Picture.bat to wallp.bat

--- wallp.bat
+++ wallp.bat
@@ -13,7 +13,8 @@ for %%F in ("%kitFolder%\*.bmp") do (
 
 :found
 :: Set wallpaper using PowerShell (works for BMP)
-powershell -Command "Add-Type -TypeDefinition \"using System; using System.Runtime.InteropServices; public class Wallpaper { [DllImport(\"user32.dll\")] public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni); }\" ; [Wallpaper]::SystemParametersInfo(20, 0, '%wallpaperFile%', 3)" >nul 2>&1
+powershell -Command "Add-Type -TypeDefinition \"using System; using System.Runtime.InteropServices; public class Wallpaper { [DllImport(\"\"user32.dll\"\")] public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni); }\" ; [Wallpaper]::SystemParametersInfo(20, 0, '%wallpaperFile%', 3)" >nul 2>&1
 
 :: Force Windows to apply changes
+timeout /t 1 /nobreak >nul
 rundll32.exe user32.dll, UpdatePerUserSystemParameters >nul 2>&1
